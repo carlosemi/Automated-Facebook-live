620d969600571b94ffa38ac6295952321afbdc66
Carlos E. Guerra <cguerra@SCBC.LOCAL>
2025-03-07 08:59:57 -0600

    Changed scheduled hour back to 7:00

--- scheduler.py
+++ scheduler.py
@@ -6,8 +6,8 @@ def run_script():
     print("Running Facebook Live Script...")
     subprocess.run(["py", "obs_connect.py"])
 
-# Schedule the script to run daily at 7:05 AM
-schedule.every().day.at("08:46").do(run_script)
+# Schedule the script to run daily at 7:00 AM
+schedule.every().day.at("07:00").do(run_script)
 
 while True:
     schedule.run_pending()
